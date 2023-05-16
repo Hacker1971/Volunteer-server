20e30dcee8ec845a7b73cb2467a559c99a3a3554
tanvir-islam <2020tanvir19711@gmail.com>
2023-05-16 13:12:05 +0600

    text

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-const app = express()
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fr7a0ud.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,59 +16,82 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // n
+    await client.connect();
     const serviceCollection = client.db("volunteers").collection("volunteer");
     const bookingCollection = client.db("volunteers").collection("bookings");
 
+    //serch ber
+    
+    // // Creating index on two fields
+    // const indexKeys = { title: 1 }; // Replace field1 and field2 with your actual field names
+    // const indexOptions = { name: "titleCategory" }; // Replace index_name with the desired index name
+    // const result = await serviceCollection.createIndex(indexKeys, indexOptions);
+    app.get("/services/:text", async (req, res) => {
+      const text = req.params.text;
+      console.log(text);
+      const result = await serviceCollection
+        .find({
+          $or: [{ title: { $regex: text, $options: "i" } }],
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    // end
+
     app.get("/services", async (req, res) => {
-        const cursor = serviceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      });
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.post("/services", async (req, res) => {
-        const service = req.body;
-        const result = await serviceCollection.insertOne(service);
-        res.send(result);
-      });
-      app.delete("/services/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await serviceCollection.deleteOne(query);
-        res.send(result);
-      });
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    });
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
 
-      //booking 
-      //http://localhost:5000/bookings?email=tanvir
-      app.get("/bookings", async (req, res) => {
-        let query = {};
-        if (req.query?.email) {
-          query = { email: req.query.email };
-        }
-        const result = await bookingCollection.find(query).toArray();
-        res.send(result);
-      });
-      app.post("/bookings", async (req, res) => {
-        const booking = req.body;
-        console.log(booking);
-        const result = await bookingCollection.insertOne(booking);
-        res.send(result);
-      });
-      app.delete("/bookings/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await bookingCollection.deleteOne(query);
-        res.send(result);
-      });
+    //booking
+    //http://localhost:5000/bookings?email=tanvir
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -78,11 +99,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Volunteer server is running...')
-})
+app.get("/", (req, res) => {
+  res.send("Volunteer server is running...");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
